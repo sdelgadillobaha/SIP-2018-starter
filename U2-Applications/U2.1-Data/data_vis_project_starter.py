fe40d85b36d23c74fd33a3bb3c7cad302d5e98ab
sdelgadillobaha <shirleydelgadillo16@gmail.com>
2019-07-01 14:51:14 -0700

    !

--- U2-Applications/U2.1-Data/data_vis_project_starter.py
+++ U2-Applications/U2.1-Data/data_vis_project_starter.py
@@ -14,7 +14,57 @@ tweetData = json.load(tweetFile)
 tweetFile.close()
 
 # Continue your program below! 
+tweettext = []
+tweetstring = ""
+for tweet in tweetData:
+    # print(tweet.keys())
+    # tweettext.append(tweet['text'])
+    tweetstring += tweet['text']
+# print(tweettext)
+print(tweetstring)
+polarity = []
+subjectivity = []
+
+for tweet in tweettext:
+    tb = TextBlob(tweet)
+    subjectivity.append(tb.subjectivity)
+    polarity.append(tb.polarity)
+
+# print(polarity)
+# print(subjectivity)
+
+
+# avg_p = sum(polarity) / len(polarity)
+# avg_s = sum(subjectivity) / len(subjectivity)
+# print(avg_p)
+# print(avg_s)
+
+# import matplotlib.pyplot as plt
+
+# plt.hist(polarity, bins=[-1, -0.5, 0.0, 0.5, 1])
+# plt.hist(subjectivity, bins=[-1, -0.5, 0.0, 0.5, 1])
+# plt.xlabel('Values')
+# plt.ylabel('Number of Items')
+# plt.title('Histogram')
+# plt.axis([-1.1, 1.1, 0, 106])
+# plt.grid(True)
+# plt.show()
+
 
 # Textblob sample:
-tb = TextBlob("You are a brilliant computer scientist.")
-print(tb.polarity)
+# tb = TextBlob("You are a brilliant computer scientist.")
+# print(tb.sentiment)
+
+
+# tweettext = []
+# tweetstring = ''
+# for tweet in tweetData:
+#     tweetstring += tweet['text']
+
+# print(tweeststring)
+tweetBlob = TextBlob(tweetstring)
+word_dict = {}
+for word in tweetBlob.words:
+    word_dict[word.lower()] = tweetBlob.word_counts[word.lower()]
+
+print(word_dict)
